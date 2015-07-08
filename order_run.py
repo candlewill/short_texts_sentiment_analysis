442eff0674b58080513663c6cd0d3fcdc521dda6
candlewill <yunchaohe@gmail.com>
2015-07-08 19:24:33 +0800

    automatic diff windows and ubuntu

--- order_run.py
+++ order_run.py
@@ -2,5 +2,21 @@ __author__ = 'NLP-PC'
 import feature_generating
 import classifiers
 import analysis
+from load_data import load_train_data
+from load_data import load_test_data
+from save_data import dump_picle
+from vectorizers import TFIDF_estimator, anew_estimator
+from analysis import analysis_result
+from classifiers import mNB
 
-# As if name==main is added, can not use to run module directly
+print('Start')
+vectorizer = anew_estimator()
+train_type = 'Sentiment140'
+texts, train_labels = load_train_data(train_type)
+transformed_train = vectorizer.fit_transform(texts)
+testdata, true_labels = load_test_data()
+transformed_test = vectorizer.transform(testdata)
+
+predict = mNB(transformed_train, train_labels, transformed_test)
+
+analysis_result(predict, true_labels)
--- vectorizers.py
+++ vectorizers.py
@@ -6,6 +6,7 @@ from preprocess import preprocessor
 from parameters import parameters
 from logger_manager import log_state
 
+
 def punctuation_estimator():
     # the method to design is referenced the site: http://goo.gl/BDe8Cb
     def get_punctuations(text):
@@ -19,27 +20,32 @@ def punctuation_estimator():
     v = TfidfVectorizer(analyzer=analyzer, binary=True, norm='l1', use_idf=False, sublinear_tf=True, max_df=0.5)
     return v
 
-def anew_estimator():
-    words, _, _ =load_extend_anew()
-    return TfidfVectorizer(max_features=1000, vocabulary=words, binary=True, norm='l1', use_idf=False, sublinear_tf=True, max_df=0.5)
 
+def anew_estimator(words=None):
+    # Note: the max_features parameter is ignored if vocabulary is not None
+    vectorizer = TfidfVectorizer(vocabulary=words, binary=True, norm='l1', use_idf=False,
+                           sublinear_tf=True, max_df=0.5)
+    return vectorizer
 
 
 def TFIDF_estimator():
     log_state('Start generating features')
+
     class StemmedTfidfVectorizer(TfidfVectorizer):
         def build_analyzer(self):
             # 利用NLTK进行词干化处理
             english_stemmer = nltk.stem.SnowballStemmer('english')
             analyzer = super(TfidfVectorizer, self).build_analyzer()
             return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
+
     vectorizer_param = {'preprocessor': preprocessor, 'ngram_range': parameters['ngram_range'], 'analyzer': 'word',
-                    'min_df': parameters['min_df'], 'max_df': parameters['max_df'],
-                    'binary': parameters['TF_binary'], 'norm': parameters['norm'],
-                    'sublinear_tf': parameters['sublinear_tf'], 'max_features': parameters['max_features']}
+                        'min_df': parameters['min_df'], 'max_df': parameters['max_df'],
+                        'binary': parameters['TF_binary'], 'norm': parameters['norm'],
+                        'sublinear_tf': parameters['sublinear_tf'], 'max_features': parameters['max_features']}
     log_state((sorted(list(vectorizer_param.items()))))
     log_state('Training data size: ' + str(parameters['test_data_size']))
     return StemmedTfidfVectorizer(**vectorizer_param)
+
 
 if __name__ == "__main__":
     d = ['thank you! why are you googd???, and!, are you good$!@#$%^&*?(), ~$%^^*%$#???']
